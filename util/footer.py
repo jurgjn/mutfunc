c3d75d324cec24cb468f5c615825e09eb9f293f5
DennisGankin <dgankin@ethz.ch>
2024-12-16 18:29:16 +0100

    citation?

--- util/footer.py
+++ util/footer.py
@@ -6,3 +6,8 @@ st.markdown('---')
 # Contact Section
 st.write("#### Contact")
 st.write("Prof. Dr. Pedro Beltrao  \n[E-Mail](mailto:beltrao@imsb.biol.ethz.ch)  \nOtto-Stern-Weg 3, 8093 Zurich, Switzerland")  
+
+
+# maybe second column with 
+# If you use the mutfunc resource in your work, please do cite us
+#Wagih O, Galardini M, Busby BP, Memon D, Typas A, Beltrao P. A resource of variant effect predictions of single nucleotide variants in model organisms. Mol Syst Biol. 2018 Dec 20;14(12):e8430.
